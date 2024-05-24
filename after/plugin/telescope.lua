4a70c0f3d5e150e016eede81820fe502876f29a0
Kiko Carisse <kiko@mediabot.ca>
2024-05-24 16:47:36 -0600

    Add some Prime Remaps

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -1,8 +1,21 @@
 local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
-vim.keymap.set('n', '<leader><C-p>', builtin.git_files, {})
+vim.keymap.set('n', '<C-p>', builtin.git_files, {})
 vim.keymap.set('n', '<leader>ps', function()
 	builtin.grep_string({ search = vim.fn.input("Grep > ") });
 end)
 vim.keymap.set('n', '<leader>pg', builtin.live_grep, {})
 
+vim.opt.smartindent = true
+
+vim.opt.wrap = false
+
+vim.opt.swapfile = false
+vim.opt.backup = false
+vim.opt.undodir = vim.fn.expand('$HOME/.vim/undodir')
+vim.opt.undofile = true
+
+vim.opt.hlsearch = false
+vim.opt.incsearch = true
+
+vim.opt.scrolloff = 8
--- lua/kiko/init.lua
+++ lua/kiko/init.lua
@@ -1,1 +1,2 @@
 require("kiko.remap")
+require("kiko.set")
