c622caf7b294ab9076b719a1c8571ef5887b389a
kikoking1 <kiko@mediabot.ca>
2024-06-03 23:37:02 -0600

    Add Autocompletions/Snippets

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,30 +1,28 @@
 return {
-    {
-    'nvim-telescope/telescope.nvim', tag = '0.1.6',
-      dependencies = { 'nvim-lua/plenary.nvim' },
-    config = function()
+	{
+		"nvim-telescope/telescope.nvim",
+		tag = "0.1.6",
+		dependencies = { "nvim-lua/plenary.nvim" },
+		config = function()
+			local builtin = require("telescope.builtin")
 
-      local builtin = require("telescope.builtin")
-
-      vim.keymap.set('n', '<leader>sf', builtin.find_files, {})
-      vim.keymap.set('n', '<leader>sg', builtin.live_grep, {})
-      vim.keymap.set('n', '<leader>pv', ':Ex<CR>', {})
-
-    end
-    },
-    {
-        'nvim-telescope/telescope-ui-select.nvim',
-        config = function()
-            require("telescope").setup({
-                extensions = {
-                    ["ui-select"] = {
-                        require("telescope.themes").get_dropdown {
-
-                        }
-                    }
-                }
-            })
-            require("telescope").load_extension("ui-select")
-        end
-    }
+			vim.keymap.set("n", "<leader>sf", builtin.find_files, {})
+			vim.keymap.set("n", "<leader>sg", builtin.live_grep, {})
+			vim.keymap.set("n", "<leader>pv", ":Ex<CR>", {})
+			vim.keymap.set("n", "<leader><leader>", builtin.oldfiles, {})
+		end,
+	},
+	{
+		"nvim-telescope/telescope-ui-select.nvim",
+		config = function()
+			require("telescope").setup({
+				extensions = {
+					["ui-select"] = {
+						require("telescope.themes").get_dropdown({}),
+					},
+				},
+			})
+			require("telescope").load_extension("ui-select")
+		end,
+	},
 }
