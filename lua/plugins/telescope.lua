3c3608841895880505b510d2017001fb1b39378a
kikoking1 <kiko@mediabot.ca>
2024-06-04 21:10:47 -0600

    Add Some Remaps

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -6,11 +6,11 @@ return {
 		config = function()
 			local builtin = require("telescope.builtin")
 
-			vim.keymap.set("n", "<leader>sf", builtin.find_files, {})
-			vim.keymap.set("n", "<leader>sg", builtin.live_grep, {})
+			vim.keymap.set("n", "<leader>sF", builtin.find_files, { desc = "[S]earch [F]iles All" })
+			vim.keymap.set("n", "<leader>sf", builtin.git_files, { desc = "[S]earch [F]iles Git" })
+			vim.keymap.set("n", "<leader>sg", builtin.live_grep, { desc = "[S]earch [G]rep" })
+			vim.keymap.set("n", "<leader>sk", builtin.keymaps, { desc = "[S]earch [K]eymaps" })
 			vim.keymap.set("n", "<leader><leader>", builtin.oldfiles, {})
-       vim.keymap.set('n', '<leader>sk', builtin.keymaps, { desc = '[S]earch [K]eymaps' })
-      vim.keymap.set('n', '<C-p>', builtin.git_files, {})
 		end,
 	},
 	{
