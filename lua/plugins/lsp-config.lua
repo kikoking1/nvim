ab9a5b478d4bc567addcc718a3f667cb2d136e92
kikoking1 <kiko@mediabot.ca>
2024-09-24 22:36:47 -0600

    Add omnisharp

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -23,6 +23,17 @@ return {
       lspconfig.ts_ls.setup({
         capabilities = capabilities
       })
+      lspconfig.omnisharp.setup({
+        cmd = { "omnisharp" },
+        filetypes = { "cs", "vb" },  -- C# and Visual Basic
+        root_dir = lspconfig.util.root_pattern("*.sln", "*.csproj"),
+        on_attach = function(client, bufnr)
+          -- Any custom configurations or keymaps
+          vim.api.nvim_buf_set_keymap(bufnr, 'n', '<space>rn', '<cmd>lua vim.lsp.buf.rename()<CR>', {})
+        end,
+        capabilities = vim.lsp.protocol.make_client_capabilities()
+        --capabilities = capabilities
+      })
       lspconfig.solargraph.setup({
         capabilities = capabilities
       })
@@ -36,6 +47,7 @@ return {
       vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
       vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
       vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, {})
+      vim.keymap.set("n", "<leader>gi", vim.lsp.buf.implementation, {})
       vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
     end,
   },
