049c17cbdea399ad0efaceddc36e1111aa551d40
kikoking1 <kiko@mediabot.ca>
2025-08-16 13:28:03 -0600

    Change color scheme to tokyo night

---
+++ lua/plugins/color-scheme.lua
@@ -0,0 +1,12 @@
+return {
+	{
+		"folke/tokyonight.nvim",
+		lazy = false,
+		name = "tokyonight",
+		priority = 1000,
+		config = function()
+			require("tokyonight").setup()
+			vim.cmd.colorscheme("tokyonight")
+		end,
+	},
+}
