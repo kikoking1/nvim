a3ba15b0b52427a98f31ac5354250334a88c4bcf
kikoking1 <kiko@mediabot.ca>
2025-08-21 20:08:35 -0600

    Add Telescope Prepopulate With Selected Text

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -6,9 +6,28 @@ return {
 		config = function()
 			local builtin = require("telescope.builtin")
 
-			vim.keymap.set("n", "<leader>fF", builtin.git_files, { desc = "[S]earch [F]iles Git" })
-			vim.keymap.set("n", "<C-p>", builtin.find_files, { desc = "[S]earch [F]iles All" })
+			local function get_visual_selection()
+				vim.cmd('noau normal! "vy"')
+				local text = vim.fn.getreg("v")
+				vim.fn.setreg("v", {})
+
+				text = string.gsub(text, "\n", "")
+				if #text > 0 then
+					return text
+				else
+					return ""
+				end
+			end
+
+			local function grep_string_visual()
+				local text = get_visual_selection()
+				builtin.live_grep({ default_text = text })
+			end
+
+			-- vim.keymap.set("n", "<leader>fF", builtin.git_files, { desc = "[S]earch [F]iles Git" })
+			vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "[S]earch [F]iles All" })
 			vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "[S]earch [G]rep" })
+			vim.keymap.set("v", "<leader>fg", grep_string_visual, { desc = "[S]earch [G]rep with selection" })
 			vim.keymap.set("n", "<leader>fk", builtin.keymaps, { desc = "[S]earch [K]eymaps" })
 			vim.keymap.set("n", "<leader>fr", builtin.oldfiles, {})
 		end,
