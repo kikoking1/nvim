00410bcd5c83fb9c64e9f84640b3eb871fc33791
kikoking1 <kiko@mediabot.ca>
2025-08-24 03:21:57 -0600

    Add Description To Remap

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -7,7 +7,7 @@ return {
 		config = function()
 			require("gitsigns").setup()
 
-			vim.keymap.set("n", "<leader>gh", ":Gitsigns preview_hunk<CR>", {})
+			vim.keymap.set("n", "<leader>gh", ":Gitsigns preview_hunk<CR>", { desc = "[G]it Preview [H]unk" })
 			vim.keymap.set("n", "<leader>gb", ":Gitsigns toggle_current_line_blame<CR>", {})
 		end,
 	},
