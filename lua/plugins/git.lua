95875ebc646a2917e6fcdf4577df4d4eba9eecb6
kikoking1 <kiko@mediabot.ca>
2025-08-24 03:56:59 -0600

    Add Diff Remap

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -1,6 +1,10 @@
 return {
 	{
 		"tpope/vim-fugitive",
+		config = function()
+			vim.keymap.set("n", "<leader>gs", ":Git<CR>", { desc = "[G]it [S]tatus" })
+			vim.keymap.set("n", "<leader>gd", ":Gvdiffsplit<CR>", { desc = "[G]it [D]iff" })
+		end,
 	},
 	{
 		"lewis6991/gitsigns.nvim",
@@ -8,7 +12,12 @@ return {
 			require("gitsigns").setup()
 
 			vim.keymap.set("n", "<leader>gh", ":Gitsigns preview_hunk<CR>", { desc = "[G]it Preview [H]unk" })
-			vim.keymap.set("n", "<leader>gb", ":Gitsigns toggle_current_line_blame<CR>", {})
+			vim.keymap.set(
+				"n",
+				"<leader>gb",
+				":Gitsigns toggle_current_line_blame<CR>",
+				{ desc = "[G]it Toggle [Blame]" }
+			)
 		end,
 	},
 }
