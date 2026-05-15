c2d83dbb61551132daecb27cfb4af1f1e4c2878e
kiko king <kiko@mediabot.ca>
2026-05-14 20:56:54 -0600

    Adding git diffview plugin

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -16,4 +16,21 @@ return {
       { "<leader>gb", "<cmd>Gitsigns toggle_current_line_blame<cr>", desc = "[G]it toggle [B]lame" },
     },
   },
+  {
+    "sindrets/diffview.nvim",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    cmd = { "DiffviewOpen", "DiffviewClose", "DiffviewToggleFiles", "DiffviewFileHistory" },
+    keys = {
+      { "<leader>gv", "<cmd>DiffviewOpen<cr>", desc = "[G]it diff[V]iew open" },
+      { "<leader>gV", "<cmd>DiffviewClose<cr>", desc = "[G]it diff[V]iew close" },
+      { "<leader>gl", "<cmd>DiffviewFileHistory<cr>", desc = "[G]it [L]og (repo history)" },
+      { "<leader>gL", "<cmd>DiffviewFileHistory %<cr>", desc = "[G]it [L]og (current file)" },
+    },
+    opts = {
+      enhanced_diff_hl = true,
+      view = {
+        merge_tool = { layout = "diff3_mixed" },
+      },
+    },
+  },
 }
